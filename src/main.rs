9307951e12c4ba154b686e355224aebe901582a1
Wendell Smith <wackywendell@gmail.com>
2021-12-12 23:03:28 -0500

    Hello world - works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+async fn hello_world() -> foundationdb::FdbResult<()> {
+    let db = foundationdb::Database::default()?;
+
+    // write a value
+    let trx = db.create_trx()?;
+    trx.set(b"hello", b"world"); // errors will be returned in the future result
+    trx.commit().await?;
+
+    // read a value
+    let trx = db.create_trx()?;
+    let maybe_value = trx.get(b"hello", false).await?;
+    let value = maybe_value.unwrap(); // unwrap the option
+
+    assert_eq!(b"world", &value.as_ref());
+
+    Ok(())
 }
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let network = unsafe { foundationdb::boot() };
+
+    hello_world().await?;
+
+    drop(network);
+    Ok(())
+}
+
+// fn main() {
+//     tokio::runtime::Builder::hello_world().await?;
+// }
